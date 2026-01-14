9201c11d7620d98e6dc68d8ab59245e98dd4b8ba
Patrick Damme <patrick.damme@tu-berlin.de>
2026-01-14 21:55:50 +0100

    [BUGFIX] Passing DaphneContext to map-kernel.
    - Background:
      - All DAPHNE runtime kernels (except for the createDaphneContext-kernel) expect a DaphneContext as their last argument.
      - As the DaphneContext is a rather low-level entity, we don't want it to be an argument of the high-level DaphneIR ops (from linear/relational algebra etc.).
      - Instead, RewriteToCallKernelOpPass appends the DaphneContext to the arguments of each CallKernelOp it creates when rewriting more high-level DaphneIR ops.
        - The only exception to this rule are DaphneIR ops that are lowered to CallKernelOp in a later pass.
        - Currently, this affects only VectorizedPipelineOp and MapOp, which are both lowered to CallKernelOp in LowerToLLVMPass.
    - Problem:
      - The map-kernel did never receive the DaphneContext as a parameter, because the lowering from MapOp to CallKernelOp in LowerToLLVMPass did not take the DaphneContext into account.
      - As a consequence, the map-kernel could see a nullptr (or maybe even some invalid pointer) as the DaphneContext, which could result in segfaults.
      - Hence, the map-kernel could not reliably access, e.g., the DaphneUserConfig.
      - In fact, when trying simple DaphneDSL scripts, it turned out that the map-kernel usually does receive the correct pointer for the DaphneContext.
        - However, this can only be due to some undefined behavior (e.g., correct pointer still at that memory address from some earlier kernel call).
        - In a WIP feature implementation we saw cases where the map-kernel sees nullptr for the DaphneContext.
    - Solution:
      - This commit solves this problem by applying the same approach we use for VectorizedPipelineOp to MapOp.
        - MapOp explicitly gets the DaphneContext as an argument; this argument can be nullptr initially, but is set to the correct DaphneContext in RewriteToCallKernelOpPass.
        - LowerToLLVMPass rewrites MapOp to a CallKernelOp with the DaphneContext as the last argument.
      - No new test cases, because whether this bug results in an error seems to depend on undefined behavior that is hard to test.
    - Thanks to @gghsu for making me aware of this bug.

--- src/parser/daphnedsl/DaphneDSLBuiltins.cpp
+++ src/parser/daphnedsl/DaphneDSLBuiltins.cpp
@@ -1357,7 +1357,7 @@ antlrcpp::Any DaphneDSLBuiltins::build(mlir::Location loc, const std::string &fu
         mlir::Attribute attr = co.getValue();
 
         return static_cast<mlir::Value>(
-            builder.create<MapOp>(loc, source.getType(), source, attr.dyn_cast<mlir::StringAttr>()));
+            builder.create<MapOp>(loc, source.getType(), source, llvm::dyn_cast<mlir::StringAttr>(attr), nullptr));
     }
 
     // ****************************************************************************
